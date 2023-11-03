37577725e333729d91201fa8cabdd9a48fe53618
jihwan2843 <jihwan2843@gmail.com>
2023-11-03 14:17:59 +0900

    clock

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
+const clock = document.querySelector(".clock");
 
+function getTime() {
+  const date = new Date();
+  let amPm = "AM";
+
+  //let hour = String(date.getHours()).padStart(2, "0");
+  let hour = date.getHours();
+  if (hour > 12) {
+    amPm = "PM";
+  }
+  if (hour > 12) {
+    hour %= 12;
+  }
+  // 삼항연산자  let hour = date.getHour() >= 13 ? date.getHour() : date.getHour() %12
+  let minutes = String(date.getMinutes()).padStart(2, "0");
+  let seconds = String(date.getSeconds()).padStart(2, "0");
+
+  clock.innerText = `${amPm} ${hour}:${minutes}:${seconds}`;
+}
+
+getTime();
+setInterval(getTime, 1000);
